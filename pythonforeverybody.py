ba777b49d50b6249ce7a02b05f4fbe0b69d40735
gtkobex-kalu <tgkalu71@gmail.com>
2026-08-26 07:33:38 +0300

    Add files via upload

---
+++ pythonforeverybody.py
@@ -0,0 +1,18 @@
+#===========================================================================================================================================================================================
+#CHAPTER ONE
+#================================================================================================================================================================================================================
+''' 
+#1 c) Store information for the long term, even beyond a power cycle.
+#2 program is a sequence of instructions that can be executed by a computer.
+#3 The difference between a compiler and an interpreter is that a compiler translates the entire program into machine code before execution, while an interpreter translates and executes the program line by line.
+#4 a) python interpreter.
+#5 It contains to problems. first problem is that primt is not difined which means print functions is misspelled. second problem is that parenthesis was expected after the function print.
+#6 a) Main Memory.
+#7 b) 44
+#8 (1) Central processing unit in human we know that main engine is our brain so does CPU is in computers.
+   (2) Main memory I don't which part of our brain stores memory but that part does as the main memory of computers store memory. 
+   (3) Secondary memory we can consider this as text book which is external memory for humans. 
+   (4) Input device these are similar to humans sence of organs such as eye, ear, nose and so on...
+   (5) Output device and we perform output by our phisical and voice via mouth.
+# The best way to avoid syntax error is trying not to make errors and you do this by running one code at a time instead of writing codes and staring at the screen to find out where is the proplem. sometimes error occurs even in very few codes so you debug the error based on the messege you have got and sharing it to others like friends or AI assistant if you can't find it. I follow this method in every day to day life.
+'''
