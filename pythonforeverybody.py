ae231b923f4b126cddb216278d050f16e145e4a2
Kalab Tsegaluel Abay <tgkalu71@gmail.com>
2026-08-27 23:07:33 +0300

    Add Chapter Two with programming examples
    Added Chapter Two with examples of variable assignments, user input, and temperature conversion.

--- pythonforeverybody.py
+++ pythonforeverybody.py
@@ -16,3 +16,33 @@
    (5) Output device and we perform output by our phisical and voice via mouth.
 # The best way to avoid syntax error is trying not to make errors and you do this by running one code at a time instead of writing codes and staring at the screen to find out where is the proplem. sometimes error occurs even in very few codes so you debug the error based on the messege you have got and sharing it to others like friends or AI assistant if you can't find it. I follow this method in every day to day life.
 '''
+#======================================================================================================================================================================================================================================================================================================================================================================================================================================================================================
+#CHAPTER TWO
+#============================================================================================================================================================================================================================================================================================================================================================================================================================================================================================
+#1
+5
+x = 5
+x + 1
+#2
+name = input("What is your name: \n")
+print(f"hello, {name}.")
+#3
+hours = input("Enter Hours: \n")
+rate = input("Enter Rate: \n")
+pay = float(hours) * float(rate)
+print(f"Pay: {pay}")
+#4
+width = 17
+height = 12.0
+n_1 = width//2
+n_2 = width/2.0
+n_3 = height/3
+n_4 = 1 + 2 * 5
+print(n_1, type(n_1))
+print(n_2, type(n_2))
+print(n_3, type(n_3))
+print(n_4, type(n_4))
+#5
+celsius = float(input("Enter temperature in Celsius: \n"))
+fahrenheit = (celsius * 9/5) + 32
+print(f"Temperature in Fahrenheit: {fahrenheit}")
